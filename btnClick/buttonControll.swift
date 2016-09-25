35d17768a49d111ad033ea45003daeb80b4f8971
sgtmt <ainnuen@gmail.com>
2016-09-26 01:25:12 +0900

    scrollviewが現状では使わないと判断したので消した

---
+++ btnClick/buttonControll.swift
@@ -0,0 +1,14 @@
+//
+//  buttonControll.swift
+//  btnClick
+//
+//  Created by 長沢　遼 on 2016/09/26.
+//  Copyright © 2016年 Sgtmt. All rights reserved.
+//
+
+import Foundation
+//buttonを管理する
+
+func xb(){
+    
+}
